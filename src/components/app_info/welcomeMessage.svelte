c77185110614196ff8e2749fe2559950aed036b7
Lisa Gillfrost <lisa.gillfrost@gmail.com>
2025-01-20 19:34:22 +0100

    create welcome message

---
+++ src/components/app_info/welcomeMessage.svelte
@@ -0,0 +1,58 @@
+<script>
+</script>
+
+<div class="container">
+    <div class="title">
+        <h1>Welcome to my <b>Portfolio</b></h1>
+    </div>
+
+    <div class="text-content">
+        <p>
+            On this page I am showcasing projects I have created, designed and
+            developed. Click the start menu button to read about them.
+        </p>
+    </div>
+    <div></div>
+</div>
+
+<style>
+    .container {
+        height: 100%;
+        width: 85%;
+        align-items: flex-start;
+    }
+    .text-content {
+        display: flex;
+        background-color: #d2d2a9;
+        border-left: 2px solid #656363;
+        border-top: 2px solid #656363;
+        border-right: 2px solid #eae9e9;
+        border-bottom: 2px solid #eae9e9;
+        width: 40em;
+        height: 20em;
+        justify-content: center;
+        align-items: center;
+    }
+    .title {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    h1 {
+        font-family: "Segoe UI", serif;
+        font-weight: 600;
+    }
+    b {
+        font-family: "Segoe UI", sans-serif;
+        font-weight: 800;
+    }
+    p {
+        font-family: "Segoe UI", sans-serif;
+        font-weight: 200;
+        font-size: medium;
+        margin: 0;
+        margin-left: 30px;
+        margin-right: 30px;
+    }
+</style>
